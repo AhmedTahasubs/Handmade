01d0918cddcefddb821a9aa206fc25df3083c141
AhmedTahasubs <ahmedtahaamuhammed@gmail.com>
2025-08-12 19:14:51 +0300

    fixed colors,id in orders

--- src/app/sellerPages/orders-management/orders-management.ts
+++ src/app/sellerPages/orders-management/orders-management.ts
@@ -310,14 +310,14 @@ console.log('Available items:', this.items.map(i => i.CustomerOrderId));
     return new Date(date).toLocaleDateString(this.currentLanguage);
   }
 
-  getStatusColor(status: OrderStatus): string {
+  getStatusColor(status: OrderStatus|string): string {
   switch (status) {
     case 'Pending': return 'bg-yellow-100 text-yellow-800 dark:bg-yellow-900/50 dark:text-yellow-400';
     case 'Shipped': return 'bg-blue-100 text-blue-800 dark:bg-blue-900/50 dark:text-blue-400';
     case 'Delivered': return 'bg-green-100 text-green-800 dark:bg-green-900/50 dark:text-green-400';
     case 'Rejected': return 'bg-red-100 text-red-800 dark:bg-red-900/50 dark:text-red-400';
     default: 
-      const exhaustiveCheck: never = status;
+      const exhaustiveCheck: any = status;
       return 'bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300';
   }
 }
